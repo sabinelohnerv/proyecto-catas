3adf51911da9019b7a6333242b46da4c9b704633
DiegoPV12 <155575455+DiegoAPV12@users.noreply.github.com>
2024-03-25 13:52:17 -0400

    Event Details

--- lib/view_models/admin_event_details_viewmodel.dart
+++ lib/view_models/admin_event_details_viewmodel.dart
@@ -3,6 +3,15 @@ import 'package:catas_univalle/views/select_judges_view.dart';
 import 'package:flutter/material.dart';
 
 class AdminEventDetailsViewModel extends ChangeNotifier {
+  Event? _event;
+
+  Event? get event => _event;
+
+  void loadEventDetails(Event event) {
+    _event = event;
+    notifyListeners();
+  }
+
   void navigateToSelectJudges(BuildContext context, Event event) {
     Navigator.push(
       context,
