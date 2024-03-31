7c924e02db15ab0e600e5d6448440b6136f4d85d
sabinelohnerv <sabinelohnerv@gmail.com>
2024-03-31 18:25:44 -0400

    Edit TextFormField Corrections

--- lib/widgets/register/custom_editfield.dart
+++ lib/widgets/register/custom_editfield.dart
@@ -1,30 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class CustomTextFormField extends StatelessWidget {
+class CustomEditFormField extends StatelessWidget {
   final String labelText;
   final void Function(String?)? onSaved;
   final String? Function(String?)? validator;
   final bool obscureText;
-  final TextEditingController? controller;
   final TextInputType keyboardType;
   final bool readOnly;
   final void Function()? onTap;
   final List<TextInputFormatter>? inputFormatters;
   final int maxLines;
+  final String initialValue;
 
-  const CustomTextFormField({
+  const CustomEditFormField({
     super.key,
     required this.labelText,
     this.onSaved,
     this.validator,
     this.obscureText = false,
     this.keyboardType = TextInputType.text,
-    this.controller,
     this.readOnly = false,
     this.maxLines = 1,
     this.onTap,
     this.inputFormatters,
+    this.initialValue = '',
   });
 
   @override
@@ -39,11 +39,11 @@ class CustomTextFormField extends StatelessWidget {
         onSaved: onSaved,
         validator: validator,
         obscureText: obscureText,
-        controller: controller,
         readOnly: readOnly,
         onTap: onTap,
         inputFormatters: inputFormatters,
         maxLines: maxLines,
+        initialValue: initialValue,
       ),
     );
   }
