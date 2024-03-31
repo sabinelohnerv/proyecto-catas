cfa7741842e47a987be598eb1fbac782fa5475fa
DiegoPV12 <155575455+DiegoAPV12@users.noreply.github.com>
2024-03-31 15:03:14 -0400

    Update judges_section.dart

--- lib/widgets/event_details/judges_section.dart
+++ lib/widgets/event_details/judges_section.dart
@@ -17,8 +17,8 @@ class JudgesSection extends StatelessWidget {
     return SingleChildScrollView(
       padding: const EdgeInsets.symmetric(vertical: 30),
       child: Wrap(
-        spacing: 8, // Space between chips
-        runSpacing: 8, // Space between lines
+        spacing: 8,
+        runSpacing: 8,
         children: judges
             .map((judge) => Chip(
                   avatar: CircleAvatar(
