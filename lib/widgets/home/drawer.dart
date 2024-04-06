90e00d61efc4bf9f35020bdd834155e0a2035768
DiegoPV12 <155575455+DiegoAPV12@users.noreply.github.com>
2024-04-05 21:02:35 -0400

    Drawer Changes

--- lib/widgets/home/drawer.dart
+++ lib/widgets/home/drawer.dart
@@ -1,5 +1,7 @@
+import 'package:catas_univalle/views/profile_view.dart';
+import 'package:catas_univalle/widgets/home/drawer_body.dart';
+import 'package:catas_univalle/widgets/home/drawer_header.dart';
 import 'package:flutter/material.dart';
-import '../../views/profile_view.dart';
 
 class CustomDrawer extends StatelessWidget {
   final String fullName;
@@ -18,60 +20,25 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          buildDrawerHeader(context),
-          ListTile(
-            leading: const Icon(Icons.account_circle),
-            title: const Text('Profile'),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const ProfileView()));
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.mail),
-            title: const Text('Contact'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: const Icon(Icons.info),
-            title: const Text('Information'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: const Icon(Icons.logout),
-            title: const Text('Log Out'),
-            onTap: onSignOut,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget buildDrawerHeader(BuildContext context) {
-    return Container(
-      color: Theme.of(context).primaryColor,
-      padding: EdgeInsets.only(
-          top: 30 + MediaQuery.of(context).padding.top, bottom: 30),
       child: Column(
         children: [
-          CircleAvatar(
-            radius: 52,
-            backgroundColor: Theme.of(context).primaryColorLight,
-            backgroundImage: NetworkImage(imageUrl),
+          DrawerHeaderWidget(
+            fullName: fullName,
+            email: email,
+            imageUrl: imageUrl,
           ),
-          const SizedBox(height: 10),
-          Text(
-            fullName,
-            style: const TextStyle(
-                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-          const SizedBox(height: 5),
-          Text(
-            email,
-            style: const TextStyle(fontSize: 16, color: Colors.white),
+          DrawerBodyWidget(
+            onProfileTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ProfileView()),
+            ),
+            onContactTap: () {
+              // Handle contact tap
+            },
+            onInformationTap: () {
+              // Handle information tap
+            },
+            onSignOut: onSignOut,
           ),
         ],
       ),
