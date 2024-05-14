d04dd04d7bbb8dafd14364ca923dd6b22f6b421a
Pablo Toledo <pali.toledo.b@gmail.com>
2024-05-14 14:00:54 -0400

    Added SearchBar for AdminTrainingEventsView

--- lib/view_models/admin_training_events_viewmodel.dart
+++ lib/view_models/admin_training_events_viewmodel.dart
@@ -1,65 +1,51 @@
-import 'package:catas_univalle/views/training_list_view.dart';
-import 'package:flutter/material.dart';
-import 'package:catas_univalle/models/event.dart';
-import '/services/training_service.dart';
 import 'dart:async';
+
+import 'package:catas_univalle/services/training_service.dart';
+import 'package:flutter/material.dart';
+import 'package:catas_univalle/views/training_list_view.dart';
+import 'package:catas_univalle/models/event.dart';
 
 class AdminTrainingEventsViewModel with ChangeNotifier {
   List<Event> _events = [];
-  List<Event> _filteredEvents = [];
   Map<String, int> trainingCounts = {};
   bool isLoading = true;
   final TrainingService trainingService;
   StreamSubscription? _subscription;
   String _searchQuery = '';
 
-  List<Event> get events => _searchQuery.isEmpty ? _events : _filteredEvents;
-
   AdminTrainingEventsViewModel({required this.trainingService}) {
-    listenToEventTrainingCounts();
+    _fetchEvents();
   }
 
-  void listenToEventTrainingCounts() {
+  List<Event> get events => _searchQuery.isEmpty
+      ? _events
+      : _events.where((event) => event.name.toLowerCase().contains(_searchQuery.toLowerCase())).toList();
+
+  void _fetchEvents() {
     isLoading = true;
     notifyListeners();
     _subscription?.cancel();
-    _subscription = trainingService
-        .fetchAllEventsWithTrainingCountsStream()
-        .listen((eventCounts) {
+    _subscription = trainingService.fetchAllEventsWithTrainingCountsStream().listen((eventCounts) {
       _events = eventCounts.keys.toList();
       trainingCounts = eventCounts.map((e, count) => MapEntry(e.id, count));
       isLoading = false;
       notifyListeners();
     }, onError: (error) {
-      print('Error listening to event trainings: $error');
+      print('Error fetching events: $error');
       isLoading = false;
       notifyListeners();
     });
   }
 
   void setSearchQuery(String query) {
-    _searchQuery = query;
-    filterEvents();
+    _searchQuery = query.toLowerCase();
     notifyListeners();
   }
 
-  void filterEvents() {
-    _filteredEvents = _events.where((event) {
-      return event.name.toLowerCase().contains(_searchQuery.toLowerCase());
-    }).toList();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      notifyListeners();
-    });
-  }
-
   void goToTrainingsListView(BuildContext context, Event event) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) =>
-            TrainingListView(eventId: event.id, isAdmin: true),
-      ),
-    );
+    Navigator.push(context, MaterialPageRoute(
+      builder: (context) => TrainingListView(eventId: event.id, isAdmin: true),
+    ));
   }
 
   @override
--- lib/views/admin_training_events_view.dart
+++ lib/views/admin_training_events_view.dart
@@ -1,4 +1,4 @@
-import 'package:catas_univalle/services/training_service.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:catas_univalle/models/event.dart';
@@ -9,30 +9,32 @@ class AdminTrainingEventsView extends StatefulWidget {
   const AdminTrainingEventsView({super.key});
 
   @override
-  State<AdminTrainingEventsView> createState() =>
-      _AdminTrainingEventsViewState();
+  State<AdminTrainingEventsView> createState() => _AdminTrainingEventsViewState();
 }
 
 class _AdminTrainingEventsViewState extends State<AdminTrainingEventsView> {
   final TextEditingController _searchController = TextEditingController();
+  Timer? _debounce;
 
   @override
   void initState() {
     super.initState();
     _searchController.addListener(_onSearchChanged);
-    Provider.of<AdminTrainingEventsViewModel>(context, listen: false)
-        .listenToEventTrainingCounts();
   }
 
   void _onSearchChanged() {
-    Provider.of<AdminTrainingEventsViewModel>(context, listen: false)
-        .setSearchQuery(_searchController.text);
+    if (_debounce?.isActive ?? false) _debounce!.cancel();
+    _debounce = Timer(const Duration(milliseconds: 500), () {
+      Provider.of<AdminTrainingEventsViewModel>(context, listen: false)
+          .setSearchQuery(_searchController.text);
+    });
   }
 
   @override
   void dispose() {
     _searchController.removeListener(_onSearchChanged);
     _searchController.dispose();
+    _debounce?.cancel();
     super.dispose();
   }
 
@@ -43,75 +45,62 @@ class _AdminTrainingEventsViewState extends State<AdminTrainingEventsView> {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<AdminTrainingEventsViewModel>(
-      create: (context) => AdminTrainingEventsViewModel(
-        trainingService: Provider.of<TrainingService>(context, listen: false),
-      ),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Todas las Capacitaciones',
-              style: TextStyle(color: Colors.white)),
-          backgroundColor: Theme.of(context).primaryColor,
-          centerTitle: true,
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
-            onPressed: () => _goBack(context),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todas las Capacitaciones', style: TextStyle(color: Colors.white)),
+        backgroundColor: Theme.of(context).primaryColor,
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: () => _goBack(context),
         ),
-        body: Consumer<AdminTrainingEventsViewModel>(
-          builder: (context, viewModel, child) {
-            if (viewModel.isLoading) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            if (viewModel.events.isEmpty) {
-              return const Center(
-                  child: Text('No hay capacitaciones disponibles.'));
-            }
-            return Column(
-              children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                  child: TextField(
-                    controller: _searchController,
-                    decoration: InputDecoration(
-                      hintText: "Buscar eventos",
-                      prefixIcon: const Padding(
-                        padding: EdgeInsets.only(left: 8),
-                        child: Icon(
-                          Icons.search,
-                          size: 22,
-                        ),
-                      ),
-                      isDense: true,
-                      contentPadding: const EdgeInsets.symmetric(vertical: 3),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(color: Colors.grey.shade300),
-                      ),
+      ),
+      body: Consumer<AdminTrainingEventsViewModel>(
+        builder: (context, viewModel, child) {
+          if (viewModel.isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (viewModel.events.isEmpty) {
+            return const Center(child: Text('No hay capacitaciones disponibles.'));
+          }
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                child: TextField(
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    hintText: "Buscar eventos",
+                    prefixIcon: const Padding(
+                      padding: EdgeInsets.only(left: 8),
+                      child: Icon(Icons.search, size: 22),
+                    ),
+                    isDense: true,
+                    contentPadding: const EdgeInsets.symmetric(vertical: 3),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(color: Colors.grey.shade300),
                     ),
                   ),
                 ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: viewModel.events.length,
-                    itemBuilder: (context, index) {
-                      Event event = viewModel.events[index];
-                      int numberOfTrainings =
-                          viewModel.trainingCounts[event.id] ?? 0;
-                      return TrainingEventCard(
-                        event: event,
-                        numberOfTrainings: numberOfTrainings,
-                        onTap: () =>
-                            viewModel.goToTrainingsListView(context, event),
-                      );
-                    },
-                  ),
-                ),
-              ],
-            );
-          },
-        ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: viewModel.events.length,
+                  itemBuilder: (context, index) {
+                    Event event = viewModel.events[index];
+                    int numberOfTrainings = viewModel.trainingCounts[event.id] ?? 0;
+                    return TrainingEventCard(
+                      event: event,
+                      numberOfTrainings: numberOfTrainings,
+                      onTap: () => viewModel.goToTrainingsListView(context, event),
+                    );
+                  },
+                )
+              ),
+            ],
+          );
+        },
       ),
     );
   }
