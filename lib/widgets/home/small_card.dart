4af6a15f0a418db1e1b4396af1c9e72d12b7f0cd
Pablo Toledo <pali.toledo.b@gmail.com>
2024-05-28 17:03:23 -0400

    LayoutBuilder in small_card

--- lib/widgets/home/small_card.dart
+++ lib/widgets/home/small_card.dart
@@ -24,36 +24,58 @@ class SmallCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: isClickable ? onTap ?? () => _handleTap(context) : null,
-      child: Card(
-        child: SizedBox(
-          width: width,
-          height: height,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/$img.png',
-                  width: 80,
-                  height: 80,
-                  fit: BoxFit.cover,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double responsiveWidth = width;
+        double responsiveHeight = height;
+        double responsiveFontSize = fontSize;
+
+        if (constraints.maxWidth < 600) {
+          responsiveWidth = width * 0.8;
+          responsiveHeight = height * 0.8;
+          responsiveFontSize = fontSize * 0.8;
+        } else if (constraints.maxWidth < 1200) {
+          responsiveWidth = width * 1.0;
+          responsiveHeight = height * 1.0;
+          responsiveFontSize = fontSize * 1.0;
+        } else {
+          responsiveWidth = width * 1.2;
+          responsiveHeight = height * 1.2;
+          responsiveFontSize = fontSize * 1.2;
+        }
+
+        return InkWell(
+          onTap: isClickable ? onTap ?? () => _handleTap(context) : null,
+          child: Card(
+            child: SizedBox(
+              width: responsiveWidth,
+              height: responsiveHeight,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/$img.png',
+                      width: responsiveWidth * 0.5,
+                      height: responsiveHeight * 0.5,
+                      fit: BoxFit.cover,
+                    ),
+                    const SizedBox(height: 12),
+                    Text(
+                      title.toUpperCase(),
+                      style: TextStyle(
+                        fontSize: responsiveFontSize,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 12),
-                Text(
-                  title.toUpperCase(),
-                  style: TextStyle(
-                    fontSize: fontSize,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 
